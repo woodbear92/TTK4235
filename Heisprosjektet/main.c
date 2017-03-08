1ace23317a560b9b4cc0e50dd784597ecd8e7b81
Vebjørn Steinsholt <vebjornsteinsholt@vebjrns-macbook-pro.local>
2017-03-08 13:23:44 +0100

    Initial Commit

---
+++ Heisprosjektet/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Heisprosjektet
+//
+//  Created by Vebjørn Steinsholt on 08.03.2017.
+//  Copyright © 2017 Vebjørn Steinsholt. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
